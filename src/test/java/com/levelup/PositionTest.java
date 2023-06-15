024d84096d004e741604cdb283e1dc72d6f83905
Nancy Herzog <nancyd.herzog@gmail.com>
2023-06-15 19:24:24 +0000

    Added test

--- src/test/java/com/levelup/PositionTest.java
+++ src/test/java/com/levelup/PositionTest.java
@@ -17,9 +17,35 @@ public class PositionTest {
         Position position=new Position(xCoordinate,yCoordinate);
         assertNotNull(position);
         assertNotNull(position.getCoordinates());
+   }
+
+    @Test
+    public void validateCoordinates() {
+
+
+        int xCoordinate=50;
+        int yCoordinate=100;
+        Position position=new Position(xCoordinate,yCoordinate);
+        assertNotNull(position);
+        assertNotNull(position.getCoordinates());
         //assert the coordinates
         assertEquals(50, position.getCoordinates().getXCoordinate());
         assertEquals(100, position.getCoordinates().getYCoordinate());
+
+    }
+
+     @Test
+    public void validateNegativeCoordinates() {
+
+
+        int xCoordinate=-10;
+        int yCoordinate=-10;
+        Position position=new Position(xCoordinate,yCoordinate);
+        assertNotNull(position);
+        assertNotNull(position.getCoordinates());
+        //assert the coordinates
+        assertEquals(-10, position.getCoordinates().getXCoordinate());
+        assertEquals(-10, position.getCoordinates().getYCoordinate());
 
     }
     
